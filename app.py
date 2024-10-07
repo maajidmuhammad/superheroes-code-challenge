36b9013255b1d66cf9ababa6ac6672acf83909d8
maajidmuhammad <abdimohamed0742@gmail.com>
2024-10-07 15:56:54 +0300

    final commit

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from models import db, Hero, Power, HeroPower
 
-
+# Initialize Flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# Initialize Flask-Migrate for database migrations
 migrate = Migrate(app, db)
 
 db.init_app(app)
@@ -167,7 +168,7 @@ def get_hero_powers():
 
 
         
-
+# Run the Flask application on port 5550 in debug mode
 
 if __name__ == '__main__':
     app.run(port=5550, debug=True)
